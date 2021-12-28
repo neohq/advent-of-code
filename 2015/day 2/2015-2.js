86172b577df3e3bd182ef6af0bd6ad9d5b89ac7a
NeoHQ <neohqq@gmail.com>
2021-12-28 20:59:06 +0200

    finish 2015 day 2

--- 2015/day 2/2015-2.js
+++ 2015/day 2/2015-2.js
@@ -28,43 +28,57 @@ let DATA = [];
   }
 }
 
-const getSurfaceInfo = (l, w, h) => {
-  const lwSide = 2*l*w;
-  const whSide = 2*w*h;
-  const hlSide = 2*h*l;
-
-  const smallestSide = Math.min(...[
-    lwSide / 2, whSide / 2, hlSide / 2,
-  ]);
-
-  const [smallestOne, smallestTwo] = [l, w, h]
-    .sort((a, b) => a - b)
-    .slice(0, 2);
-
-  return {
-    surfaceWithSmallest: lwSide + whSide + hlSide + smallestSide,
-    perimeterOfSmallest: (smallestOne + smallestTwo) * 2,
-  }
-}
-
 /**
  * Part one
  */
 {
-  let totalFt = 0;
-  for (const box of DATA) {
-    const boxSurface = getSurfaceInfo(box.l, box.w, box.h).surfaceWithSmallest;
-    totalFt += boxSurface;
+  const getSurface = (l, w, h) => {
+    const lwSide = 2*l*w;
+    const whSide = 2*w*h;
+    const hlSide = 2*h*l;
+  
+    const smallestSide = Math.min(...[
+      lwSide / 2, whSide / 2, hlSide / 2,
+    ]);
+  
+    const [smallestOne, smallestTwo] = [l, w, h]
+      .sort((a, b) => a - b)
+      .slice(0, 2);
+  
+    return lwSide + whSide + hlSide + smallestSide;
   }
 
-  console.log(`Total surface of wrapping paper (ft): ${totalFt}`);
+  let totalPaper = 0;
+  for (const box of DATA) {
+    const boxSurface = getSurface(box.l, box.w, box.h);
+    totalPaper += boxSurface;
+  }
+
+  console.log(`Total surface of wrapping paper (ft): ${totalPaper}`);
 }
 
 /**
  * Part two
  */
 {
+  const getPerimeter = (l, w, h) => {
+    const [smallestOne, smallestTwo] = [l, w, h]
+    .sort((a, b) => a - b)
+    .slice(0, 2);
 
+
+    const perimeter = (parseFloat(smallestOne) + parseFloat(smallestTwo)) * 2;
+    const volume = l * w * h;
+
+    return perimeter + volume;
+  }
+
+  let totalRibbon = 0;
+  for (const box of DATA) {
+    totalRibbon += getPerimeter(box.l, box.w, box.h);
+  }
+
+  console.log(`Total ribbon (ft): ${totalRibbon}`);
 }
 
 
