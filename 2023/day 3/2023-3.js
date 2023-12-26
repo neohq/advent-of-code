8433f03030ed07b6f574cec2118d2901ae5ed055
neohq <neohqq@gmail.com>
2023-12-26 22:37:42 +0200

    day 3 part one

--- 2023/day 3/2023-3.js
+++ 2023/day 3/2023-3.js
@@ -7,45 +7,63 @@ const path = require('path');
 let DATA = [];
 {
   const input = fs.readFileSync(
-    path.resolve(__dirname, './2023-3-demo.txt'),
-    // path.resolve(__dirname, './2023-2-input.txt'),
+    // path.resolve(__dirname, './2023-3-demo.txt'),
+    path.resolve(__dirname, './2023-3-input.txt'),
     { encoding: 'utf8', flag: 'r' },
   );
   DATA = input.split('\n');
   DATA.pop(); // Remove last element from the input empty line (empty line)
 }
 
-const isNumber = (value) => !Number.isNaN(Number(value));
+const isCharSymbol = (char) => Number.isNaN(Number(char)) && char !== '.';
 
 /**
  * Part One
  */
 {
+  let parNumbers = [];
+  let partNumbersSum = 0;
+  let rowIndex = 0;
+
   for (const row of DATA) {
-    console.log(row);
+    // console.log(`row: ${row}`);
 
     for (const match of row.matchAll(/\d+/g)) {
-      console.log(match[0], row.indexOf(match[0]), row.lastIndexOf(match[0]));
+      const nr = match[0];
+      const startIndex = match.index;
+      const endIndex = startIndex + (nr.length - 1);
+
+      const rowsToCheck = [DATA[rowIndex-1], DATA[rowIndex], DATA[rowIndex+1]];
+      // console.log(`nr: ${nr} -`, 'rowsToCheck',  rowsToCheck);
+
+      // Iterate over top, current & bellow rows
+      for (const rowToCheck of rowsToCheck) {
+        if (!rowToCheck) continue; // Ignore out of border rows
+        // console.log(`  row: ${rowToCheck}`);
+
+        // Start at -1 and end +1 to also check chars before and after the number
+        // Prevent checking outside of row length
+        const checkStart = Math.max(startIndex - 1, 0);
+        const checkEnd = Math.min(endIndex + 1, rowToCheck.length);
+
+        for (let i = checkStart; i <= checkEnd; i++) {
+          const char = rowToCheck.charAt(i);
+          // console.log(`    char ${i}: ${char}   ${isCharSymbol(char) ? '✓': ''}`);
+
+          if (isCharSymbol(char)) {
+            // parNumbers.push(nr);
+            partNumbersSum += Number(nr);
+            break;
+          }
+        }
+
+      }
     }
 
-    for (let i = 0; i < row.length; i++) {
-      const prevChar = row.charAt(i-1);
-      const curChar = row.charAt(i);
-      const nextChar = row.charAt(i+1);
-
-      // let startNumberIndex = 0;
-      // let endNumberIndex = 0;
-      //
-      // if (isNumber(curChar) && !isNumber(prevChar)) {
-      //   startNumberIndex = i;
-      // }
-      //
-      // if (isNumber(curChar) && !isNumber(nextChar)) {
-      //   endNumberIndex = i;
-      // }
-      //
-      // console.log(startNumberIndex, endNumberIndex);
-    }
-    console.log('\n');
+    rowIndex++;
+    // console.log('\n');
   }
+
+  // console.log('Part number', parNumbers);
+  console.log('Part one:', partNumbersSum); // 520135
 }
