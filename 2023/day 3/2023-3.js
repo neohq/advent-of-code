fa35ec6fa2877a06f30b36c85fc34bc231c7e5d1
neohq <neohqq@gmail.com>
2023-12-26 23:10:42 +0200

    finish day 3

--- 2023/day 3/2023-3.js
+++ 2023/day 3/2023-3.js
@@ -7,8 +7,8 @@ const path = require('path');
 let DATA = [];
 {
   const input = fs.readFileSync(
-    // path.resolve(__dirname, './2023-3-demo.txt'),
-    path.resolve(__dirname, './2023-3-input.txt'),
+    path.resolve(__dirname, './2023-3-demo.txt'),
+    // path.resolve(__dirname, './2023-3-input.txt'),
     { encoding: 'utf8', flag: 'r' },
   );
   DATA = input.split('\n');
@@ -17,53 +17,84 @@ let DATA = [];
 
 const isCharSymbol = (char) => Number.isNaN(Number(char)) && char !== '.';
 
+let parNumbers = [];
+let partNumbersSum = 0;
+let rowIndex = 0;
+let gears = {};
+
+for (const row of DATA) {
+  // console.log(`row: ${row}`);
+
+  for (const match of row.matchAll(/\d+/g)) {
+    const nr = match[0];
+    const startIndex = match.index;
+    const endIndex = startIndex + (nr.length - 1);
+
+    const rowsIndexToCheck = [rowIndex-1, rowIndex, rowIndex+1];
+    // console.log(`nr: ${nr} -`, 'rowsToCheck',  rowsToCheck);
+
+    // Iterate over top, current & bellow rows
+    for (const rowIndexToCheck of rowsIndexToCheck) {
+      const rowToCheck = DATA[rowIndexToCheck];
+      if (!rowToCheck) continue; // Ignore out of border rows
+      // console.log(`  row: ${rowToCheck}`);
+
+      // Start at -1 and end +1 to also check chars before and after the number
+      // Prevent checking outside of row length
+      const checkStart = Math.max(startIndex - 1, 0);
+      const checkEnd = Math.min(endIndex + 1, rowToCheck.length);
+
+      for (let i = checkStart; i <= checkEnd; i++) {
+        const char = rowToCheck.charAt(i);
+        // console.log(`    char ${i}: ${char}   ${isCharSymbol(char) ? '✓': ''}`);
+
+        /**
+         * Part One
+         */
+        if (isCharSymbol(char)) {
+          // parNumbers.push(nr);
+          partNumbersSum += Number(nr);
+          // break; // TODO: If uncommenting this line produces different results then leave it uncommented for Part One
+        }
+
+        /**
+         * Part Two
+         */
+        if (char === '*') {
+          const gearPos = `r${rowIndexToCheck}-i${i}`;
+
+          if (!gears[gearPos]) {
+            gears[gearPos] = [Number(nr)];
+          } else {
+            gears[gearPos].push(Number(nr));
+          }
+        }
+      }
+
+    }
+  }
+
+  rowIndex++;
+  // console.log('\n');
+}
+
 /**
  * Part One
  */
-{
-  let parNumbers = [];
-  let partNumbersSum = 0;
-  let rowIndex = 0;
+// console.log('Part number', parNumbers);
+console.log('Part one:', partNumbersSum); // 520135
 
-  for (const row of DATA) {
-    // console.log(`row: ${row}`);
 
-    for (const match of row.matchAll(/\d+/g)) {
-      const nr = match[0];
-      const startIndex = match.index;
-      const endIndex = startIndex + (nr.length - 1);
+/**
+ * Part Two
+ */
 
-      const rowsToCheck = [DATA[rowIndex-1], DATA[rowIndex], DATA[rowIndex+1]];
-      // console.log(`nr: ${nr} -`, 'rowsToCheck',  rowsToCheck);
-
-      // Iterate over top, current & bellow rows
-      for (const rowToCheck of rowsToCheck) {
-        if (!rowToCheck) continue; // Ignore out of border rows
-        // console.log(`  row: ${rowToCheck}`);
-
-        // Start at -1 and end +1 to also check chars before and after the number
-        // Prevent checking outside of row length
-        const checkStart = Math.max(startIndex - 1, 0);
-        const checkEnd = Math.min(endIndex + 1, rowToCheck.length);
-
-        for (let i = checkStart; i <= checkEnd; i++) {
-          const char = rowToCheck.charAt(i);
-          // console.log(`    char ${i}: ${char}   ${isCharSymbol(char) ? '✓': ''}`);
-
-          if (isCharSymbol(char)) {
-            // parNumbers.push(nr);
-            partNumbersSum += Number(nr);
-            break;
-          }
-        }
-
-      }
-    }
-
-    rowIndex++;
-    // console.log('\n');
+// Filter out gears (*) with only one adjacent number
+let gearsRatiosSum = 0;
+Object.keys(gears).map((key) => {
+  if (gears[key].length >= 2) {
+    const gearRatio = gears[key].reduce((acc, val) => acc * val);
+    gearsRatiosSum += gearRatio;
   }
-
-  // console.log('Part number', parNumbers);
-  console.log('Part one:', partNumbersSum); // 520135
-}
+})
+console.log(`Part Two: ${gearsRatiosSum}`);
